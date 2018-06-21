34dfcbe18c373c238fee99bae59afb5c32d67124
shaunpearce <me@shaunpearce.ie>
2018-06-20 23:08:19 -0700

    Setting up dev tools

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menuu.</Text>
+        </View>
+      </Provider>
     );
   }
 }
---
+++ reducers/index.js
@@ -0,0 +1,13 @@
+
+const initialState = {
+  test: 1,
+}
+
+function app (state = initialState, action) {
+  switch (action.type) {
+    default :
+      return state
+  }
+}
+
+export default app
